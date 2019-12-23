e63f2a115f65be85b3df5330a851eedb6f417617
joq tyyppi <eero.ojala@helsinki.fi>
2019-12-23 17:27:16 +0200

    Added build version of frontend and downgraded some dependencies to make them vulnerable for security risks

---
+++ build/precache-manifest.245cc9179f516935aa5969469cebbdf4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "954e57765d72f8f16dc7e632c6ea5eec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db5ba3cb76a579688c16",
+    "url": "/static/js/2.33a201cd.chunk.js"
+  },
+  {
+    "revision": "44bfbd673f2c2c0fc8b1f13154cd745c",
+    "url": "/static/js/2.33a201cd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b8be29b8688ba947db49",
+    "url": "/static/js/main.066f3b59.chunk.js"
+  },
+  {
+    "revision": "e03f63f3f986f82ed2c0",
+    "url": "/static/js/runtime-main.85d31de7.js"
+  }
+]);
